dc08996cf15be2c16012b276d5278c01a084cb58
Zbigniew Czerwonka <zczerwonka@gmail.com>
2024-01-06 19:29:27 +0100

    added npm package and learned functions and objects

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 let a = 5;
 let b = 10;
 
-const sum = a+b;
+const sum = a + b;
 
 console.log("a + b = ");
 console.log(a + b);
@@ -14,7 +14,7 @@ console.log(sum)
 const arrayNumbers = [1, 2, 3, 5];
 
 // deklaracja funkcji nazwanej
-function mapperFunction( item ) {
+function mapperFunction(item) {
     return item * 2;
 }
 
@@ -26,8 +26,34 @@ console.log(mapperResult);
 
 // zadanie - mapperFunction jako funkcja nienazwana
 console.log("mapper as lambda function:");
-console.log(arrayNumbers.map(item => item *2));
+console.log(arrayNumbers.map(item => item * 2));
 
 // funkcja lambda (anonimowa) () => {}
-const filtered = mapperResult.filter( item => item > 5 )
+const filtered = mapperResult.filter(item => item > 5)
 console.log(filtered);
+
+// obiekty
+const bawara = {
+    power: {
+        value: 190,
+        label: 'Horse power',
+        units: 'hP'
+    },
+    avgConsumption: 8,
+    lastErrors: ["error1", "error2", "error 501"],
+    detailedErrors: [
+        {
+            status: "error",
+            code: "501",
+            message: "Unable to connect with engine module"
+        },
+        {
+            status: "warning",
+            code: "301",
+            message: "..."
+        }
+    ],
+    drive: () => {
+        // TODO: implement drive
+    }
+};
